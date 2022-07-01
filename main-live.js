5c066be6718357d7a61627642bc6e4510b955cc0
mxw74 <marcus.walls@utexas.edu>
2022-06-30 23:30:20 -0500

    make dropdown option and name inputs for players. Also some light wireframe looking styling.

--- main-live.js
+++ main-live.js
@@ -1,6 +1,18 @@
 'use strict';
-
 // the function that will be called by the unit test below
+
+
+function userNames() {
+    
+    let name1 = document.getElementById('player1Name').value
+    let name2 = document.getElementById('player2Name').value
+
+    document.getElementById('display1').innerHTML = name1
+    document.getElementById('display2').innerHTML = name2
+
+    // I need a connection that puts the name on the page once it has been collected via a button
+  }
+  
 const rockPaperScissors = (hand1, hand2) => {
 
     hand1 = document.getElementById('user1Input').value
@@ -15,35 +27,43 @@ const rockPaperScissors = (hand1, hand2) => {
     if ( (hand1=='rock' && hand2=='rock') || (hand1=='paper' && hand2=='paper') || (hand1=='scissors' && hand2=='scissors') ) { //both hands are ROCK - its a tie
   
       console.log("It's a tie!");
-      document.getElementById('display').innerHTML = "It's a tie!";
+      document.getElementById('display').innerHTML = "You tied!";
     
     } else if ( (hand1=='rock' && hand2=='scissors') || (hand1 =='scissors' && hand2=='paper') || (hand1 =='paper' && hand2 =='rock') ) {
   
       console.log('Player 1 Wins!');
-      document.getElementById('display').innerHTML = 'Player 1 Wins!';
+      document.getElementById('display').innerHTML = 'Player 1!';
   
     } else if ( (hand1=='scissors' && hand2=='rock') || (hand1 =='paper' && hand2=='scissors') || (hand1 =='rock' && hand2 =='paper') ) {
   
       console.log('Player 2 Wins!');
-      document.getElementById('display').innerHTML = 'Player 2 Wins!';
+      document.getElementById('display').innerHTML = 'Player 2!';
   
   } else {
     // getPrompt()
-    document.getElementById('display').innerHTML = 'Invalid. Please select: rock, paper, or scissors for each input'
+    clearDisplay = document.getElementById('display').innerHTML = 'Invalid. Please select: rock, paper, or scissors for each input'
     
     // .alert("This is invalid. Please select rock, paper, or scissors")
   }
   
   } // end of function 
+
+  function clearDisplay() {
+   document.getElementById("display").innerHTML= "Winner Goes First!"
+  }
+
   
+
+
   // the first function called in the program to get an input from the user
   // to run the function use the command: node main.js
   // to close it ctrl + C
-  function getPrompt() {
-    rl.question('hand1: ', (answer1) => {
-      rl.question('hand2: ', (answer2) => {
-        console.log( rockPaperScissors(answer1, answer2) );
-        getPrompt();
-      });
-    });
-  }
+//   function getPrompt() {
+//     rl.question('hand1: ', (answer1) => {
+//       rl.question('hand2: ', (answer2) => {
+//         console.log( rockPaperScissors(answer1, answer2) );
+//         getPrompt();
+//       });
+//     });
+//   }
+
